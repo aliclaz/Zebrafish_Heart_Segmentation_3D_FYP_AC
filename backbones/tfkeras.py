0fd9d9402343edd1367651c5c04e39e1002826d3
aliclaz <alastair.claridge01@gmail.com>
2023-05-30 15:47:01 +0100

    'Added script for importing tfkeras and producing' 'model and its preprocessing'

---
+++ backbones/tfkeras.py
@@ -0,0 +1,8 @@
+import tensorflow.keras as tfkeras
+from .models_factory import ModelFactory
+
+class TFKerasModelsFactory(ModelsFactory):
+    def get_kwargs():
+        return {'backend': tfkeras.backend, 'layers': tfkeras.layers, 'models': tfkeras.models, 'utils': tfkeras.utils}
+    
+Classifiers = TFKerasModelsFactory()
