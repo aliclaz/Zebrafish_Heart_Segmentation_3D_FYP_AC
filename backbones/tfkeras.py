5bf921e012dccb7d155532ff939402e9229356d1
aliclaz <alastair.claridge01@gmail.com>
2023-05-30 16:04:53 +0100

    'Debug'

--- backbones/tfkeras.py
+++ backbones/tfkeras.py
@@ -1,6 +1,7 @@
 import tensorflow.keras as tfkeras
 from .models_factory import ModelFactory
 
+@staticmethod
 class TFKerasModelsFactory(ModelsFactory):
     def get_kwargs():
         return {'backend': tfkeras.backend, 'layers': tfkeras.layers, 'models': tfkeras.models, 'utils': tfkeras.utils}
