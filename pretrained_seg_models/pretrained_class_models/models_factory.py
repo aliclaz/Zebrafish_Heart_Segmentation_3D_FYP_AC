18dac86d0c98ce012e00b7b4b6df3028e41d904a
aliclaz <alastair.claridge01@gmail.com>
2023-06-02 14:36:03 +0100

    Debug of models factory

--- pretrained_seg_models/pretrained_class_models/models_factory.py
+++ pretrained_seg_models/pretrained_class_models/models_factory.py
@@ -12,30 +12,30 @@ class ModelsFactory:
         'vgg16': [vgg16.VGG16, vgg16.preprocess_input]
     }
 
-@property
-def models(self):
-    return self._models
+    @property
+    def models(self):
+        return self._models
 
-def models_names(self):
-    return list(self.models.keys())
+    def models_names(self):
+        return list(self.models.keys())
 
-@staticmethod
-def get_kwargs():
-    return {}
+    @staticmethod
+    def get_kwargs():
+        return {}
 
-def inject_submodules(self, func):
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        modules_kwargs = self.get_kwargs()
-        new_kwargs = dict(list(kwargs.items()) + list(modules_kwargs.items()))
+    def inject_submodules(self, func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            modules_kwargs = self.get_kwargs()
+            new_kwargs = dict(list(kwargs.items()) + list(modules_kwargs.items()))
 
-        return func(*args, **new_kwargs)
+            return func(*args, **new_kwargs)
 
-    return wrapper
+        return wrapper
 
-def get(self, name):
-    model_fn, preprocess_input = self.models[name]
-    model_fn = self.inject_submodules(model_fn)
-    preprocess_input = self.inject_submodules(preprocess_input)
+    def get(self, name):
+        model_fn, preprocess_input = self.models[name]
+        model_fn = self.inject_submodules(model_fn)
+        preprocess_input = self.inject_submodules(preprocess_input)
 
-    return model_fn, preprocess_input
+        return model_fn, preprocess_input
