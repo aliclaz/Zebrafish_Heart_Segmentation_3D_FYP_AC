7c3a16df8c9e7aa935c986d74126bca2fa702854
aliclaz <alastair.claridge01@gmail.com>
2023-06-03 23:23:46 +0100

    Debug

--- pretrained_seg_models/models/atten_res_unet.py
+++ pretrained_seg_models/models/atten_res_unet.py
@@ -78,7 +78,7 @@ def AttentionBlock(inter_shape, use_batchnorm, name=None):
                                 name=name, **kwargs)(sigmoid_xg)
         upsample_psi = RepeatElement(upsample_psi, shape_x[4], name=name)
 
-        y = Mult(**kwargs)(upsample_psi, skip_connection, name=name)
+        y = Mult(**kwargs, name=name)(upsample_psi, skip_connection)
 
         result = Conv3DBn(shape_x[4], (1, 1, 1), kernel_initializer='he_normal', padding='same', use_batchnorm=True, name=name, **kwargs)(y)
         
