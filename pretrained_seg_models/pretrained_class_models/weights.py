6844115b087d4b7001cae6629bfe1df5128305a7
aliclaz <alastair.claridge01@gmail.com>
2023-06-03 19:46:37 +0100

    Added error messages

--- pretrained_seg_models/pretrained_class_models/weights.py
+++ pretrained_seg_models/pretrained_class_models/weights.py
@@ -18,9 +18,18 @@ def load_model_weights(model, model_name, dataset, classes, include_top, **kwarg
     if weights:
         weights = weights[0]
 
+        if include_top and weights['classes'] != classes:
+            raise ValueError('If using `weights` and `include_top`'
+                             ' as true, `classes` should be {}'.format(weights['classes']))
+
         weights_path = keras_utils.get_file(weights['name'], weights['url'], cache_subdir='models', md5_hash=weights['md5'])
 
         model.load_weights(weights_path)
+
+    else:
+        raise ValueError('There is no weights for such configuration: ' +
+                         'model = {}, dataset = {}, '.format(model.name, dataset) +
+                         'classes = {}, include_top = {}.'.format(classes, include_top))
 
 WEIGHTS_COLLECTION = [
     {
