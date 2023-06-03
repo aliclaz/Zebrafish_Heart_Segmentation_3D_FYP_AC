391d5dfcca719940fd0e2f3ef1084531d0de18aa
aliclaz <alastair.claridge01@gmail.com>
2023-06-03 20:05:27 +0100

    Deleting function that was present twice

--- pretrained_seg_models/models/atten_res_unet.py
+++ pretrained_seg_models/models/atten_res_unet.py
@@ -93,72 +93,12 @@ def DecoderBlock(filters, stage, use_batchnorm=False):
     def layer(input_tensor, skip=None):
         x = GatingSignal(filters, use_batchnorm, name=gate_name)(input_tensor)
         if skip is not None:
-            atten = AttentionBlock(filters, use_batchnorm, name=gate_name)(skip, x)
+            atten = AttentionBlock(filters, use_batchnorm, name=atten_name)(skip, x)
         x = UpSamp3D(size=(), name=up_name, **kwargs)(input_tensor)
         if skip is not None:
             x = layers.Concatenate(axis=4, name=concat_name)([x, atten])
         
         x = ResConvBlock(filters, use_batchnorm, name=res_conv_block_name)(x)
-
-        return x
-
-    return layer
-
-def AttentionBlock(inter_shape, use_batchnorm, name=None):
-    kwargs = get_submodules()
-
-    def wrapper(skip_connection, gating):
-        shape_x = backend.int_shape(x)
-        shape_g = backend.int_shape(gating)
-
-        theta_x = Conv3DBn(inter_shape, kernel_size=(2, 2, 2), strides=(2, 2, 2), padding='same', kernel_initalizer='he_normal',
-                           use_batchnorm=use_batchnorm, name=name, **kwargs)(skip_connection)
-        shape_theta_x = backend.int_shape(theta_x)
-
-        phi_g = Conv3DBn(inter_shape, kernel_size=(1, 1, 1), padding='same', kernel_initalizer='he_normal',
-                         use_batchnorm=use_batchnorm, name=name, **kwargs)(gating)
-        upsample_g = Conv3DTrans(inter_shape, (3, 3, 3), padding='same', strides=(shape_theta_x[1] // shape_g[1],
-                                                                                 shape_theta_x[2] // shape_g[2],
-                                                                                 shape_theta_x[3] // shape_g[3]),
-                                                                                 name=name, **kwargs)(phi_g)
-        
-        act_xg = AddAct('relu', name=name, **kwargs)([upsample_g, theta_x])
-        sigmoid_xg = Conv3DBn(1, kernel_size=(1, 1, 1), activation='softmax', kernel_initializer='he_normal', padding='same',
-                              use_batchnorm=use_batchnorm, name=name, **kwargs)(act_xg)
-        shape_sigmoid = backend.int_shape(sigmoid_xg)
-        upsample_psi = UpSamp3D(size=(shape_x[1] // shape_sigmoid[1], 
-                                       shape_x[2] // shape_sigmoid[2], 
-                                       shape_x[3] // shape_sigmoid[3]), name=name, **kwargs)(sigmoid_xg)
-        upsample_psi = RepeatElement(upsample_psi, shape_x[4])
-
-        y = Mult([upsample_psi, skip_connection])
-
-        result = Conv3DBn(shape_x[4], (1, 1, 1), kernel_intializer='he_normal', padding='same', use_batchnorm=True, name=name, **kwargs)(y)
-        
-        return result
-    
-    return wrapper
-
-def DecoderBlock(filters, stage, use_batchnorm=False):
-    kwargs = get_submodules()
-
-    gate_name = 'decoder_stage{}_gating'.format(stage)
-    atten_name = 'decoder_stage{}_attention'.format(stage)
-    up_name = 'decoder_stage{}_upsampling'.format(stage)
-    conv1_name = 'decoder_stage{}a'.format(stage)
-    conv2_name = 'decoder_stage{}b'.format(stage)
-    concat_name = 'decoder_stage{}_concat'.format(stage)
-
-    def layer(input_tensor, skip=None):
-        x = GatingSignal(filters, use_batchnorm, name=gate_name)(input_tensor)
-        if skip is not None:
-            atten = AttentionBlock(filters, use_batchnorm, name=gate_name)(skip, x)
-        x = UpSamp3D(size=(), name=up_name, **kwargs)(input_tensor)
-        if skip is not None:
-            x = layers.Concatenate(axis=4, name=concat_name)([x, atten])
-        
-        x = Conv3x3BnReLU(filters, use_batchnorm, name=conv1_name)(x)
-        x = Conv3x3BnReLU(filters, use_batchnorm, name=conv2_name)(x)
 
         return x
 
