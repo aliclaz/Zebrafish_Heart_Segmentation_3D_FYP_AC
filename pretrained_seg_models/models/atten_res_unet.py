7221f234b280c19c0f401de7ac5c77b1a8c5d2de
aliclaz <alastair.claridge01@gmail.com>
2023-06-03 22:09:42 +0100

    Trying something

--- pretrained_seg_models/models/atten_res_unet.py
+++ pretrained_seg_models/models/atten_res_unet.py
@@ -69,9 +69,9 @@ def AttentionBlock(inter_shape, use_batchnorm, name=None):
         sigmoid_xg = Conv3DBn(1, kernel_size=(1, 1, 1), activation='softmax', kernel_initializer='he_normal', padding='same',
                               use_batchnorm=use_batchnorm, name=name, **kwargs)(act_xg)
         shape_sigmoid = backend.int_shape(sigmoid_xg)
+        print(shape_x[1] // shape_sigmoid[1], shape_x[2] // shape_sigmoid[2], shape_x[3] // shape_sigmoid[3])
         upsample_psi = UpSamp3D(size=(shape_x[1] // shape_sigmoid[1], shape_x[2] // shape_sigmoid[2], shape_x[3] // shape_sigmoid[3]), 
                                 name=name, **kwargs)(sigmoid_xg)
-        print(upsample_psi.shape[0])
         upsample_psi = RepeatElement(upsample_psi, shape_x[4], name=name)
 
         y = Mult([upsample_psi, skip_connection])
@@ -139,7 +139,7 @@ def build_atten_res_unet(backbone, skip_connection_layers, decoder_filters=(256,
 
     return model
 
-def AttentionResUnet(backbone_name='vgg16', input_shape=(None, None, 3), classes=1, activation='sigmoid', weights=None, encoder_weights='imagenet', 
+def AttentionResUnet(backbone_name='vgg16', input_shape=(None, None, None, 3), classes=1, activation='sigmoid', weights=None, encoder_weights='imagenet', 
                      encoder_freeze=False, encoder_features='default', decoder_filters=(256, 128, 64, 32, 16,), decoder_use_batchnorm=True, dropout=None, **kwargs):
     global backend, layers, models, keras_utils
     backend, layers, models, keras_utils = get_submodules_from_kwargs(kwargs)
