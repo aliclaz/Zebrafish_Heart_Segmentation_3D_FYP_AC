5f0c9033d30dd2db441283d28fa7d83162f9eda5
aliclaz <alastair.claridge01@gmail.com>
2023-06-04 02:23:50 +0100

    Changing data type of train_masks

--- train_model.py
+++ train_model.py
@@ -49,6 +49,7 @@ def main(args):
     # Load the training masks and images into the code and preprocess both datasets
 
     x_train, x_val, y_train, y_val = load_process_imgs(img_path, mask_path)
+    print(np.unique(y_train))
 
     # Define model parameters
 
@@ -59,7 +60,7 @@ def main(args):
 
     opt = Adam(args.learning_rate)
 
-    train_masks = np.concatenate((y_train.astype(np.uint8), y_val.astype(np.uint8)), axis=0)
+    train_masks = np.concatenate((y_train, y_val), axis=0)
     flat_train_masks = train_masks.reshape(-1)
     class_weights = compute_class_weight('balanced', classes=np.unique(flat_train_masks), y=flat_train_masks)
     print(class_weights)
