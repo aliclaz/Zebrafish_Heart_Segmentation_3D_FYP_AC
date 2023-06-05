03d45b7f8a4c94d4991ae1bc4541fa7980847711
aliclaz <alastair.claridge01@gmail.com>
2023-06-05 17:35:31 +0100

    debug

--- imgPreprocessing.py
+++ imgPreprocessing.py
@@ -35,7 +35,7 @@ def load_process_imgs(img_path, mask_path, split, n_classes):
     for i in range(n_classes):
         mask_channels[i] = np.array(mask_channels[i])
         mask_channel_patches = patchify(mask_channels[i], (64, 64, 64), step=64)
-        mask_channel_patches = np.expands_dims(mask_channel_patches, axis=5)
+        mask_channel_patches = np.expand_dims(mask_channel_patches, axis=5)
         mask_channels_patches.append(mask_channel_patches)
         if i == 0:
             mask_patches = mask_channels_patches[i]
