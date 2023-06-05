537ec004fc9083976d403a8228d105d61ed65750
aliclaz <alastair.claridge01@gmail.com>
2023-06-05 02:44:58 +0100

    debug

--- seg_models/pretrained_class_models/__init__.py
+++ seg_models/pretrained_class_models/__init__.py
@@ -1,4 +1,4 @@
-import . as ka
+from . import _KERAS_BACKEND, _KERAS_LAYERS, _KERAS_UTILS, _KERAS_MODELS
 
 def get_submodules_from_kwargs(kwargs):
     backend = kwargs.get('backend', ka._KERAS_BACKEND)
