a93809716a1865919a2647dfefc336ec88c836d8
aliclaz <alastair.claridge01@gmail.com>
2023-06-05 18:52:24 +0100

    Testing

--- imgPreprocessing.py
+++ imgPreprocessing.py
@@ -30,6 +30,7 @@ def load_process_imgs(img_path, mask_path, split, n_classes):
     for i in range(256):
         for j in range(n_classes):
             temp_mask = mask[:,:,i,j]
+            print(temp_mask)
             temp_mask[temp_mask != 0] = (j + 1)*(1 / (255 // 6))
             print(np.unique(temp_mask))
             mask_channels[j].append(temp_mask)
