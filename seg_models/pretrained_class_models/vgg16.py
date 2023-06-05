9824534ce9321238f72e90e73bf6b8224a529221
aliclaz <alastair.claridge01@gmail.com>
2023-06-05 03:32:12 +0100

    debug

--- seg_models/pretrained_class_models/vgg16.py
+++ seg_models/pretrained_class_models/vgg16.py
@@ -1,9 +1,9 @@
 import os
 from .. import get_submodules_from_kwargs
-from . import imagenet_utils
+from .imagenet_utils import preprocess_input as ppi
 from .weights import load_model_weights
 
-preprocess_input = imagenet_utils.preprocess_input
+preprocess_input = ppi
 
 def VGG16(include_top=False, weights='imagenet', input_tensor=None, input_shape=None, pooling=None, classes=1000, stride_size=2, 
           init_filters=64, max_filters=512, repetitions=(2, 2, 3, 3, 3), **kwargs):
