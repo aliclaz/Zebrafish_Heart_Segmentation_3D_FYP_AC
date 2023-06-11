ed67105f29d6eb64ea8d029a045b20072645084e
aliclaz <alastair.claridge01@gmail.com>
2023-06-11 14:42:08 +0100

    Debug

--- train_model.py
+++ train_model.py
@@ -73,7 +73,7 @@ def main(args):
 
     print('Batch size per device: ', args.batch_size)
 
-    steps_per_epoch = (batch_size // args.batch_size)
+    steps_per_epoch = (len(x_train) // batch_size)
     print('Number of steps per epoch: ', steps_per_epoch)
 
     with strategy.scope():
