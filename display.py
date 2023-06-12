ad27d6dd8d96c83e435e02052abd1607a863d685
aliclaz <alastair.claridge01@gmail.com>
2023-06-12 20:34:02 +0100

    Debug

--- display.py
+++ display.py
@@ -42,7 +42,7 @@ def show_val_masks(model_name, backbone, imgs, gts, preds, out_path, classes, hp
     colours_normalized = colours / np.max(colours)
     c = np.stack([colours_normalized]*3, axis=-1)
 
-    fig, ax = plt.subplots(len(imgs)*3, 3, figsize=(24, 12*len(imgs)))
+    fig, ax = plt.subplots(len(imgs)*3, 3, figsize=(24, 15*len(imgs)))
 
     k = 0
     for i in range(len(imgs)):
@@ -74,7 +74,7 @@ def show_pred_masks(model_name, backbone, imgs, preds, out_path, classes, hpf):
     colours_normalized = colours / np.max(colours)
     c = np.stack([colours_normalized]*3, axis=-1)
 
-    fig, ax = plt.subplots(3*len(imgs), 2, figsize=(16, 12*len(imgs)))
+    fig, ax = plt.subplots(3*len(imgs), 2, figsize=(16, 15*len(imgs)))
 
     k = 0
     for i in range(len(imgs)):
@@ -112,7 +112,7 @@ def disp_3D_val(val_masks, val_preds, model_name, backbone, classes, out_path, h
         greyscale_colours = np.stack([colours_normalized]*3, axis=-1)
         ax.scatter(x, y, z, c=greyscale_colours, marker='s', s=cube_size**2)
         c = np.unique(greyscale_colours.ravel())
-        patches = [mpatches.Patch(ec='k', fc=fc[i-1] if i != 0 else 'k', label=classes[i]) for i in range(len(classes))]
+        patches = [mpatches.Patch(ec='k', fc=fc[i], label=classes[all_colours[i]]) for i in range(len(fc))]
         ax.legend(handles=patches, bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
 
         ax = fig.add_subplot(len(val_masks), 2, (2*i)+2, projection='3d')
@@ -124,7 +124,7 @@ def disp_3D_val(val_masks, val_preds, model_name, backbone, classes, out_path, h
         greyscale_colours = np.stack([colours_normalized]*3, axis=-1)
         ax.scatter(x, y, z, c=greyscale_colours, markers='s', s=cube_size**2)
         c = np.unique(greyscale_colours.ravel())
-        patches = [mpatches.Patch(ec='k', fc=fc[i-1] if i != 0 else 'k', label=classes[i]) for i in range(len(classes))]
+        patches = [mpatches.Patch(ec='k', fc=fc[i], label=classes[all_colours[i]]) for i in range(len(fc))]
         ax.legend(handles=patches, bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
     plt.savefig(out_path+'{}HPF_{}_{}_val_masks_and_preds3D.jpg'.format(hpf, backbone, model_name))
     plt.show()
@@ -150,7 +150,7 @@ def disp_3D_pred(preds, model_name, backbone, out_path, classes, hpf):
         greyscale_colours = np.stack([colours_normalized]*3, axis=-1)
         ax.scatter(x, y, z, c=greyscale_colours, markers='s', s=cube_size**2)
         c = np.unique(greyscale_colours.ravel())
-        patches = [mpatches.Patch(ec='k', fc=fc[i-1] if i != 0 else 'k', label=classes[i]) for i in range(len(classes))]
+        patches = [mpatches.Patch(ec='k', fc=fc[i], label=classes[all_colours[i]]) for i in range(len(fc))]
         ax.legend(handles=patches, bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
     plt.savefig(out_path+'{}HPF_{}_{}_test_preds3D.jpg'.format(hpf, backbone, model_name))
     plt.show()
