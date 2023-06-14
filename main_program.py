830bb2c2075436f0e2c99419d43943fc56218cfb
aliclaz <alastair.claridge01@gmail.com>
2023-06-14 06:16:03 +0100

    Debug

--- main_program.py
+++ main_program.py
@@ -35,6 +35,9 @@ def main(args):
 
     mod_hpf = get_hpf(args.entered_hpf)
 
+    if args.reuse == 'True':
+        reuse = True
+
     # Import images, preprocess, load_model, make predictions and save the predicted masks
 
     imgs, preds = predict(MOD_PATH+'{}HPF_{}_{}_{}epochs.h5'.format(mod_hpf, args.backbone, args.model_name, args.epochs), args.model_name, args.backbone, in_files, OUT_PATH, args.entered_hpf, GM=args.gm)
@@ -67,7 +70,7 @@ def main(args):
 
     # The results will be displayed in the terminal and saved as a CSV file in the user chose output path for access at a later date
 
-    if args.reuse:
+    if reuse:
         if args.gm == 'healthy' or 'Healthy' or 'HEALTHY' or 'None' or 'none' or 'NONE':
             add_healthy_df_calcs(preds, classes, args.entered_hpf, scales, STAT_PATH, OUT_PATH)
         else:
@@ -83,7 +86,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--in_files', type=str, help='Enter filenames of the images being input (separated by space)', required=True)
-    parser.add_argument('--reuse', action='store_true', help='Set to \'True\' if adding more images with a gm and hpf that have already been entered', default=False)
+    parser.add_argument('--reuse', type=str, help='Set to \'True\' if adding more images with a gm and hpf that have already been entered', default=False)
     parser.add_argument('--entered_hpf', type=int, help='What stage of development, in hours post-fertilisation (hpf), were the provided images taken in?', required=True)
     parser.add_argument('--gm', type=str, help='What genetic modification was applied to the zebrafish embryo prior to the image being taken?', default='Healthy')
     parser.add_argument('--scale', type=str, help='Enter the width dimension of each image in \u03bcm separated by a single space. Press ENTER when done.', required=True)
